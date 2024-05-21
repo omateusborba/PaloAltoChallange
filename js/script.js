42bdefcb28f04a688834547df0771ede55551de9
omateusborba <RM556418@fiap.com.br>
2024-05-20 22:01:58 -0300

    AHHHH

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 function confereSenha() {
     const senha = document.querySelector('input[name=senha]');
     const rsenha = document.querySelector('input[name=rsenha]')
+    const carEsp = "!@#$%&*-_=+§)({}[]/?|"
+    var n = rsenha.length
 
-    if (rsenha.value === senha.value){
-        alert('senha confere')
+
+    if (rsenha.value === senha.value) {
+        alert('dEu')
     } else {
         alert('senha não confere')
     }
-
 }
+
