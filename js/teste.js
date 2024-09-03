32c9a35c9cc9e233a9f8fd992675a9ff0cb8c67a
FlintMorais <pedromoraisaugusto7@gmail.com>
2024-09-03 20:41:28 -0300

    Videos treinamento

--- js/teste.js
+++ js/teste.js
@@ -13,3 +13,30 @@ function confereSenha() {
         alert("Deu Certo");
     }
 }
+
+$(document).ready(function(){
+    var videos = [
+        "Eh7XY7VlfAM?si=NFHHIpFpSDRa_w6A", // Vídeo 1
+        "H-0AlAN1VRY?si=oKDSaqJwd-f7loeb", // Vídeo 2
+        "tX-jJm0Pl0Q?si=TKB-J69xbp3r_fbz"  // Vídeo 3
+    ];
+    var currentIndex = 0;
+
+    function updateVideo(index) {
+        $("#youtubeVideo").attr("src", "https://www.youtube.com/embed/" + videos[index]);
+    }
+
+    $("#prox").click(function(){
+        if (currentIndex < videos.length - 1) {
+            currentIndex++;
+            updateVideo(currentIndex);
+        }
+    });
+
+    $("#voltar").click(function(){
+        if (currentIndex > 0) {
+            currentIndex--;
+            updateVideo(currentIndex);
+        }
+    });
+});
